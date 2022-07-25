5e6389318f72f9ac1f99d49cda8780d516725b1a
Luis Ramirez <luisenrique.ramirez@globant.com>
2022-07-25 16:16:07 -0500

    Addinf open closed principle

---
+++ OCP/product_correct.py
@@ -0,0 +1,90 @@
+from enum import Enum
+
+class Color(Enum):
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+
+class Size(Enum):
+    SMALL = 1
+    MEDIUM = 2
+    LARGE = 3
+
+
+class Specification:
+    # Empty classes works as interfaces
+    def is_satisfied(self, item):
+        pass
+
+    # overload of & operator
+    def __and__(self, other):
+        return AndSpecification(self, other)
+
+class Product:
+    def __init__(self, name, color, size):
+        self.name = name
+        self.color = color
+        self.size = size
+
+class Filter:
+    # general porpouse filter
+    def filter(self, items, spec):
+        pass
+
+class ColorSpecification(Specification):
+    def __init__(self, color):
+        self.color = color
+
+    def is_satisfied(self, item):
+        return item.color == self.color
+
+class SizeSpecification(Specification):
+    def __init__(self, size):
+        self.size = size
+
+    def is_satisfied(self, item):
+        return item.size == self.size
+
+class AndSpecification(Specification):
+    # Combinator
+    def __init__(self, *args):
+        self.args = args
+
+    def is_satisfied(self, item):
+        return all(map(
+            lambda spec: spec.is_satisfied(item), self.args))
+
+class BetterFilter(Filter):
+    # we are implementing the methods in Filter with out touching 
+    # the existing code
+    def filter(self, items, spec):
+        for item in items:
+            if spec.is_satisfied(item):
+                yield item
+
+apple = Product('Apple', Color.GREEN, Size.SMALL)
+tree = Product('Tree', Color.GREEN, Size.LARGE)
+house = Product('House', Color.BLUE, Size.LARGE)
+
+products = [apple, tree, house]
+
+bf = BetterFilter()
+
+print('Green products (new):')
+green = ColorSpecification(Color.GREEN)
+for p in bf.filter(products, green):
+    print(f' - {p.name} is green')
+
+print('Large products:')
+large = SizeSpecification(Size.LARGE)
+for p in bf.filter(products, large):
+    print(f' - {p.name} is large')
+
+# without & overload
+# large_blue = AndSpecification(large, ColorSpecification(Color.BLUE))
+
+# with & overload
+print('Large blue items:')
+large_blue = large & ColorSpecification(Color.BLUE)
+for p in bf.filter(products, large_blue):
+    print(f' - {p.name} is large and blue')
---
+++ OCP/product_wrong.py
@@ -0,0 +1,53 @@
+from enum import Enum
+
+"""
+If we keep adding more and more filter, and different attributes to the 
+product, this will lead us in to broke the principle
+"""
+
+class Color(Enum):
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+
+
+class Size(Enum):
+    SMALL = 1
+    MEDIUM = 2
+    LARGE = 3
+
+
+class Product:
+    def __init__(self, name, color, size):
+        self.name = name
+        self.color = color
+        self.size = size
+
+
+class ProductFilter:
+    def filter_by_color(self, products, color):
+        for p in products:
+            if p.color == color: yield p
+
+    def filter_by_size(self, products, size):
+        for p in products:
+            if p.size == size: yield p
+
+    def filter_by_size_and_color(self, products, size, color):
+        # state space explosion
+        # 3 criteria
+        # c s w cs sw cw csw = 7 methods
+        for p in products:
+            if p.color == color and p.size == size:
+                yield p
+
+apple = Product('Apple', Color.GREEN, Size.SMALL)
+tree = Product('Tree', Color.GREEN, Size.LARGE)
+house = Product('House', Color.BLUE, Size.LARGE)
+
+products = [apple, tree, house]
+
+pf = ProductFilter()
+print('Green products (old):')
+for p in pf.filter_by_color(products, Color.GREEN):
+    print(f' - {p.name} is green')
